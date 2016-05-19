9dd43025d221a94180d2c5fe057e5a76a9dbf2e3
shanks-devil <wangliang.devil@gmail.com>
2016-05-20 00:20:40 +0800

    empty request

--- src/main/java/com/github/shanks/rabbit/producer/SendMessage.java
+++ src/main/java/com/github/shanks/rabbit/producer/SendMessage.java
@@ -12,7 +12,6 @@ public class SendMessage {
 	@Autowired
 	private AmqpTemplate rabbitTemplate;
 	
-	
 	public void send(MessageModel messageModel) {
 		rabbitTemplate.convertAndSend(messageModel);
 	}
